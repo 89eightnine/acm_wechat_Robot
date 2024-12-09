186d93f331d22c257cf22d86ee26b59111b08b86
CatBiscuit <141328976+89eightnine@users.noreply.github.com>
2024-12-09 13:07:20 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import itchat, time
 from itchat.content import *
 import Getcodeforces
 import ImageRenderer
+import GetCFContest
+import CFRatingDrawer
 
 
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
@@ -11,7 +13,17 @@ def text_reply(msg):
 
 @itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], isGroupChat=True)
 def group_text_reply(msg):
-    if msg.text.startswith('#cf'):
+    if msg.text.startswith('#cfrating'):
+        Username = msg.text[9:].split()
+        if(Username[0]==''):
+            msg['User'].send("不给名字我搜个锤子")
+        else:
+            img = CFRatingDrawer.get_img(Username[0])
+            if img==None:
+                msg['User'].send(f"苦呀西 找不到{Username[0]} qwq~")
+            else:
+                msg['User'].send_image(img)
+    elif msg.text.startswith('#cf'):
         # 提取'#cf'之后的内容作为命令
         Username = msg.text[3:].split()
         # 根据命令执行相应的操作
@@ -26,15 +38,22 @@ def group_text_reply(msg):
     elif msg.text.startswith("#at"):
         msg['User'].send("TODO")
     elif msg.text.startswith('#近期比赛'):
-        msg['User'].send("TODO")
+        Resource_name = msg.text[5:].split()
+        if(Resource_name[0]=='Codeforces' or Resource_name[0]=='cf'):
+            back=GetCFContest.getCF_Contest()
+            msg['User'].send(back)
+        else:
+            msg['User'].send("TODO")
+
     elif msg.text.startswith('看看腿'):
         msg['User'].send("下头！")
 
     elif msg.text.startswith('#help'):
         msg['User'].send("1. #cf Userid 查看cf分数\n"
                          "2. #at Userid 查看atcoder分数\n"
-                         "3. #近期比赛 Userid 查看近期的比赛\n"
-                         "4. #help帮助菜单\n"
+                         "3. #cfrating Userid 查看cf rating\n"
+                         "4. #近期比赛 Userid 查看近期的比赛\n"
+                         "5. #help帮助菜单\n"
                          "灌注哈基幂谢谢喵~\n")
 
 
